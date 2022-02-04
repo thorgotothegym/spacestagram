aa1969a4c4ccd3ac3f84084ccae75440281ee29c
Thor <al3ix.vidal@gmail.com>
2022-02-04 10:51:14 +0100

    feat: create nasaphoto service

--- src/service/Nasa.tsx
+++ src/service/Nasa.tsx
@@ -2,10 +2,18 @@ import axios from "axios";
 
 import { IApod } from "./type";
 
-export const getNasa = async (count: string): Promise<IApod[]> => {
+export const getPhotosFromNasa = async (
+  start_date: string = "2022-01-01",
+  end_date: string = "2022-01-20"
+): Promise<IApod[]> => {
   const { REACT_APP_SECREET } = process.env;
   const { data } = await axios.get<IApod[]>(
-    `https://api.nasa.gov/planetary/apod?api_key=${REACT_APP_SECREET}&count=${count}`
+    `https://api.nasa.gov/planetary/apod?start_date=${start_date}&end_date=${end_date}&api_key=${REACT_APP_SECREET}`,
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
   );
   return data;
 };
