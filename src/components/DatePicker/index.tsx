6a7f452e63226e34751443313b3e402b7b5a1684
Thor <al3ix.vidal@gmail.com>
2022-02-09 13:32:39 +0100

    feat: created RangeDate component

---
+++ src/components/DatePicker/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { DatePicker } from "antd";
+import moment from "moment";
+
+const { RangePicker } = DatePicker;
+
+interface IRangeDate {
+  clickHandler: (dateStrings: Array<string>) => void;
+}
+
+export const RangeDate = ({ clickHandler }: IRangeDate): JSX.Element => {
+  const [dateCallBack, setDateCallBack] = useState<Array<string>>([""]);
+  const dateFormat = "YYYY/MM/DD";
+  const today = moment().format(dateFormat);
+
+  return (
+    <>
+      <RangePicker
+        defaultValue={[moment("2019-09-03", dateFormat), moment(today)]}
+        disabled={[false, true]}
+        onChange={(dates: any, dateStrings: Array<string>) => {
+          setDateCallBack(dateStrings);
+          clickHandler(dateStrings);
+          console.log("dates, ", dates, "dateStrings, ", dateStrings);
+        }}
+        format={dateFormat}
+      />
+    </>
+  );
+};
