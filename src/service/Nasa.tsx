14a4b5876077aee820dbbcc439105759aa07ea48
Thor <al3ix.vidal@gmail.com>
2022-02-10 09:47:07 +0100

    feat: remove moment and made more light

--- src/service/Nasa.tsx
+++ src/service/Nasa.tsx
@@ -3,12 +3,14 @@ import axios from "axios";
 import { IApod } from "./type";
 
 export const getPhotosFromNasa = async (
-  start_date: string = "2022-01-01",
-  end_date: string = "2022-01-20"
+  start_date: string = "2022-01-01"
 ): Promise<IApod[]> => {
   const { REACT_APP_SECREET } = process.env;
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  const TODAYISO = today.toISOString().slice(0, 10)
   const { data } = await axios.get<IApod[]>(
-    `https://api.nasa.gov/planetary/apod?start_date=${start_date}&end_date=${end_date}&api_key=${REACT_APP_SECREET}`,
+    `https://api.nasa.gov/planetary/apod?start_date=${start_date}&end_date=${TODAYISO}&api_key=${REACT_APP_SECREET}`,
     {
       headers: {
         "Access-Control-Allow-Origin": "*",
